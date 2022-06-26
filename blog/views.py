ab40d7c836aa27f6fc77cc6e32b6743fb0b5165a
Pouria <p.nazirian@gmail.com>
2022-06-26 21:50:30 +0400

    context
    context in view

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,5 @@ def blog_view(request):
     return render(request, 'blog/blog-home.html')
 
 def blog_single(request):
-    return render(request, 'blog/blog-single.html')
+    context = {'title' : 'bitcoin', 'content' : 'bitcoin is good'}
+    return render(request, 'blog/blog-single.html', context)
